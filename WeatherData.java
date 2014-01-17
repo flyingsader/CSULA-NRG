1e462e3264d88f2e7c56634c3193956b54a9db84
Wklein <mushroomhermit@gmail.com>
2014-01-16 20:13:43 -0800

    weather data maker

---
+++ WeatherData.java
@@ -0,0 +1,35 @@
+
+public class WeatherData {
+
+	String Locality;
+	String Timestamp;
+	int Temperature;
+	int Windspeed;
+	int WindDir;
+	int SolarRad;
+	
+	
+	public WeatherData(int day, int hour){
+		this.Locality = "Los Angeles";
+		if(hour < 10){
+			if(day < 10)
+				this.Timestamp = "2014-01-0" + Integer.toString(day) + " 0" + Integer.toString(hour) + ":00:00";
+			else
+				this.Timestamp = "2014-01-" + Integer.toString(day) + " 0" + Integer.toString(hour) + ":00:00";
+		}else{
+			if(day < 10)
+				this.Timestamp = "2014-01-0" + Integer.toString(day) + " " + Integer.toString(hour) + ":00:00";
+			else
+				this.Timestamp = "2014-01-" + Integer.toString(day) + " " + Integer.toString(hour) + ":00:00";
+		}
+		this.Temperature = (int)(Math.random() * 20 + 10);
+		this.Windspeed = (int)(Math.random() * 15);
+		this.WindDir = (int)(Math.random() * 360);
+		this.SolarRad = (int)(Math.random() * 50) + 50;
+	}
+	
+	public String toSqlEntry(){
+		return "Insert Into Devices Values('" + this.Locality + "', '" + this.Timestamp + "', " + this.Temperature + ", " + this.Windspeed + ", " + this.WindDir + ", " + this.SolarRad + ");";
+	}
+
+}

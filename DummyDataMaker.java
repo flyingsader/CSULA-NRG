4a0e10b49a322d4f952a03e3034cc85203f0d645
Wklein <mushroomhermit@gmail.com>
2014-01-16 20:16:49 -0800

    updates

--- DummyDataMaker.java
+++ DummyDataMaker.java
@@ -6,12 +6,16 @@ public class DummyDataMaker {
 		int entries = 20;
 		
 		Device device;
+		WeatherData weather;
+		GridData grid;
 		
 		
 		for(int i = 0; i < entries; i++){
 			
-			device = new Device();
-			System.out.println(device.toSqlEntry());
+			//device = new Device();
+			//weather = new WeatherData(i / 24 + 1, i % 24);
+			grid = new GridData(i / 24 + 1, i % 24);
+			System.out.println(grid.toSqlEntry());
 			
 		}
 		

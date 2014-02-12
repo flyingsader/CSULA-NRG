e1d7c1ed0fb0af0d4ddfeab9c4606dd3e3da5242
flyingsader <michael.a.holloway@outlook.com>
2014-02-11 20:51:59 -0800

    Committing updates

--- CSULA-NRG/src/functions/PF.java
+++ CSULA-NRG/src/functions/PF.java
@@ -1,7 +1,51 @@
 package functions;
 
-public class PF {
+import java.sql.SQLException;
+import java.util.List;
 
+import types.GridData;
+import types.WeatherData;
+
+public class PF {
+	
+	private DMF _dmf;
+	
+	public PF()
+	{
+		try {
+			_dmf = new DMF();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void sendWeatherDataToDMF(WeatherData weatherData)
+	{
+		_dmf.insertWeatherData(weatherData);
+	}
+	
+	public void sendGridDataToDMF(GridData gridData)
+	{
+		_dmf.insertGridData(gridData);
+	}
+	
+	public void sendGridDeficitPredictionToDMF()
+	{
+		try {
+			List<WeatherData> wData = _dmf.getAllWeatherData();
+			List<GridData> gData = _dmf.getAllGridData();
+			GridData prediction = null;
+			
+			
+			
+			_dmf.setDeficit((int) (prediction.getCapacity() - (prediction.getDemand() * 1.01)));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
 	public static void main(String[] args) {
 
 	}

6a4de5c7d0bc6d4fbe05ed0159bd80b01244f750
flyingsader <michael.a.holloway@outlook.com>
2014-02-25 18:34:48 -0800

    added constructor to weatherdata

--- CSULA-NRG/src/types/WeatherData.java
+++ CSULA-NRG/src/types/WeatherData.java
@@ -1,5 +1,12 @@
 package types;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class WeatherData {
 
 	String locality;
@@ -8,28 +15,52 @@ public class WeatherData {
 	int windspeed;
 	int windDir;
 	int solarRad;
-	
-	
-	public WeatherData(int day, int hour){
+
+	public static final String TimestampFormat = "yyyy-MM-dd hh:mm:ss:SS";
+	private static final String TimestampStringFormat = "%s-%s-%s %s:00:00:00";
+
+	public WeatherData(int day, int hour) {
 		this.locality = "Los Angeles";
-		if(hour < 10){
-			if(day < 10)
-				this.timestamp = "2014-01-0" + Integer.toString(day) + " 0" + Integer.toString(hour) + ":00:00";
+		if (hour < 10) {
+			if (day < 10)
+				this.timestamp = "2014-01-0" + Integer.toString(day) + " 0"
+						+ Integer.toString(hour) + ":00:00";
 			else
-				this.timestamp = "2014-01-" + Integer.toString(day) + " 0" + Integer.toString(hour) + ":00:00";
-		}else{
-			if(day < 10)
-				this.timestamp = "2014-01-0" + Integer.toString(day) + " " + Integer.toString(hour) + ":00:00";
+				this.timestamp = "2014-01-" + Integer.toString(day) + " 0"
+						+ Integer.toString(hour) + ":00:00";
+		} else {
+			if (day < 10)
+				this.timestamp = "2014-01-0" + Integer.toString(day) + " "
+						+ Integer.toString(hour) + ":00:00";
 			else
-				this.timestamp = "2014-01-" + Integer.toString(day) + " " + Integer.toString(hour) + ":00:00";
+				this.timestamp = "2014-01-" + Integer.toString(day) + " "
+						+ Integer.toString(hour) + ":00:00";
 		}
-		this.temperature = (int)(Math.random() * 20 + 10);
-		this.windspeed = (int)(Math.random() * 15);
-		this.windDir = (int)(Math.random() * 360);
-		this.solarRad = (int)(Math.random() * 50) + 50;
+		this.temperature = (int) (Math.random() * 20 + 10);
+		this.windspeed = (int) (Math.random() * 15);
+		this.windDir = (int) (Math.random() * 360);
+		this.solarRad = (int) (Math.random() * 50) + 50;
 	}
-	
-	public WeatherData(String loc, String time, int temp, int windsp, int windDir, int sol){
+
+	public WeatherData(int month, int day, int year, int hour) {
+		this(hour, day);
+
+		String sHour = Integer.toString(hour);
+		String sDay = Integer.toString(day);
+		String sMonth = Integer.toString(month);
+		if (hour < 10)
+			sHour = "0" + sHour;
+		if (day < 10)
+			sDay = "0" + sDay;
+
+		if (month < 10)
+			sMonth = "0" + sMonth;
+		this.timestamp = String.format(TimestampStringFormat, year, sMonth,
+				sDay, sHour);
+	}
+
+	public WeatherData(String loc, String time, int temp, int windsp,
+			int windDir, int sol) {
 		this.locality = loc;
 		this.timestamp = time;
 		this.temperature = temp;
@@ -37,7 +68,7 @@ public class WeatherData {
 		this.windDir = windDir;
 		this.solarRad = sol;
 	}
-	
+
 	public String getLocality() {
 		return locality;
 	}
@@ -85,9 +116,26 @@ public class WeatherData {
 	public void setSolarRad(int solarRad) {
 		this.solarRad = solarRad;
 	}
+	
+	public Calendar parseTimestampToDate() throws ParseException
+	{
+		DateFormat df = new SimpleDateFormat(TimestampFormat,
+				Locale.ENGLISH);
+		Date result = df.parse(timestamp);
+		Calendar time = Calendar.getInstance();
+		time.set(Calendar.MONTH, result.getMonth());
+		time.set(Calendar.DAY_OF_MONTH, result.getDay());
+		time.set(Calendar.YEAR, result.getYear());
+		time.set(Calendar.HOUR_OF_DAY, result.getHours());
+		time.set(Calendar.MINUTE, result.getMinutes());
+		return time;
+	}
 
-	public String toSqlEntry(){
-		return "Insert Into WeatherData Values('" + this.locality + "', '" + this.timestamp + "', " + this.temperature + ", " + this.windspeed + ", " + this.windDir + ", " + this.solarRad + ");";
+	public String toSqlEntry() {
+		return "Insert Into WeatherData Values('" + this.locality + "', '"
+				+ this.timestamp + "', " + this.temperature + ", "
+				+ this.windspeed + ", " + this.windDir + ", " + this.solarRad
+				+ ");";
 	}
 
 }
